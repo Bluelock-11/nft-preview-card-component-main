676d9ed8d9730e991480947101f6f2628ac2d22c
Bluelock-11 <ibeeddrick@gmail.com>
2024-09-04 16:52:22 +0100

    Modified

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ let card = document.getElementById("cardBox");
 let details = document.getElementById("detailBox");
 let buttonOne = document.getElementById("buttonOne");
 let buttonTwo = document.getElementById("buttonTwo");
+let title = document.getElementById("titleBox");
+let aB = document.getElementById("julesWyvern");
 
 function lightMode() {
   background.style.backgroundColor = "white";
   card.style.backgroundColor = "#e0e0e0";
+  title.style.color = "black";
   details.style.color = "black";
 }
 
@@ -14,6 +17,8 @@ function darkMode() {
   background.style.backgroundColor = "black";
   card.style.backgroundColor = "#222222";
   details.style.color = "white";
+  title.style.color = "white";
+  aB.style.color = "white";
 }
 
 buttonOne.addEventListener("click", lightMode);
